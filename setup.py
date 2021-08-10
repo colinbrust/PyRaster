e13b7152704a0be0d38ebfb67e93f8029bfd96c1
colinbrust <colin.brust@gmail.com>
2021-08-10 13:48:01 -0600

    Altered setup.py to properly install package data.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-data_files = [str(x) for x in pathlib.Path(here / 'data').glob('*.tif')]
+# data_files = [str(x) for x in pathlib.Path(here / 'data').glob('*.tif')]
 
 setup(
     name='PyRaster', 
@@ -17,17 +17,15 @@ setup(
     author='Colin Brust',
     author_email='colin.brust@gmail.com',
     keywords='raster, rasterio, simplified',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    packages=['pyraster'],
+    package_dir={'pyraster': 'src/pyraster'},
+    package_data={'pyraster': ['data/*.tif']},
     python_requires='>=3.6, <4',
     install_requires=['rasterio', 'numpy', 'matplotlib'],  # Optional
     # extras_require={  # Optional
     #     'dev': ['check-manifest'],
     #     'test': ['coverage'],
     # },
-    package_data={
-        '': data_files,
-    },
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
